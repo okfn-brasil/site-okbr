2f6cc3da0d962820d678b3de523e30021d4fdb13
thatielys <thatielys@DESKTOP-17GLA63.localdomain>
2021-05-28 09:58:47 -0300

    -- A ferramenta de busca só capta palavras-chave do título, e não do conteúdo das notícias - Tarefa: https://app.clickup.com/t/4z9f6g
    CU-4z9f6g

--- wp-content/themes/okbr/functions.php
+++ wp-content/themes/okbr/functions.php
@@ -346,5 +346,40 @@ date_default_timezone_set('America/Sao_Paulo');
 
 	wp_enqueue_style( 'stylesheet', get_template_directory_uri() . '/assets/style_geral.css' );
 	wp_enqueue_script( 'script_geral', get_template_directory_uri() . '/assets/script_geral.js', array('jquery'));
-	
+
+	// Search
+		function cf_search_join( $join ) {
+			global $wpdb;
+		
+			if ( is_search() ) {    
+				$join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
+			}
+		
+			return $join;
+		}
+		add_filter('posts_join', 'cf_search_join' );
+
+		function cf_search_where( $where ) {
+			global $pagenow, $wpdb;
+		
+			if ( is_search() ) {
+				$where = preg_replace(
+					"/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+					"(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+			}
+		
+			return $where;
+		}
+		add_filter( 'posts_where', 'cf_search_where' );
+
+		function cf_search_distinct( $where ) {
+			global $wpdb;
+		
+			if ( is_search() ) {
+				return "DISTINCT";
+			}
+		
+			return $where;
+		}
+		add_filter( 'posts_distinct', 'cf_search_distinct' );
 	
