de677efbef68b94f9cbb81ec342a4f231349d289
thatielys <thatielys@DESKTOP-17GLA63.localdomain>
2021-05-31 15:55:28 -0300

    - Menu do feed criado - Arrumei o responsivo no carrousel de projetos - Arrumei os icones do insta e linkdin no responsivo(rodapé)

--- wp-content/themes/okbr/assets/script_geral.js
+++ wp-content/themes/okbr/assets/script_geral.js
@@ -4,6 +4,20 @@
         infinite: true,
         slidesToShow: 3,
         centerPadding: '50px',
+        responsive:[
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 700,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+        ]
       });
     });
   })();
